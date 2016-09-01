6e0d44bf79819de0e148ddd950bfeef0fe7e73b4
Fabian Wentz <fabianwentz@gmx.de>
2016-09-01 18:41:55 +1000

    settings update

---
+++ InTouch/InTouch.Core/ViewModels/SettingsModel.cs
@@ -0,0 +1,28 @@
+﻿using MvvmCross.Core.ViewModels;
+namespace InTouch.Core.ViewModels
+{
+	public class SettingsModel
+	:MvxViewModel
+	{
+		private string notification = "default";
+		public string Notification
+		{
+			get { return notification; }
+			set { notification = value;}
+		}
+		private string vibration = "short";
+		public string Vibration
+		{
+			get { return vibration; }
+			set { vibration = value; }
+		}
+		private string light = "white";
+		public string Light
+		{
+			get { return light; }
+			set { light = value; }
+		}
+
+	}
+}
+
